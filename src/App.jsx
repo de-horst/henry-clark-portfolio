9558885b2ee37b0319d86d9089b9496e1097f6fe
Denise Horst <denisef.horst@gmail.com>
2024-09-16 21:17:42 -0300

    05 Work

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
  * @licence Apache-2.0
  */
 
+
 /**
  * COMPONENTS
  */
@@ -10,6 +11,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 const App = () => {
   return (
@@ -19,6 +21,7 @@ const App = () => {
         <Hero />
         <About />
         <Skill />
+        <Work />
       </main>
     </>
   );
